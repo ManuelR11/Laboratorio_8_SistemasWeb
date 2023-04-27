e8d6d131704fdf0c17dcc8e768da05fabf6a0c09
ManuelR11 <rod21509@uvg.edu.gt>
2023-04-26 21:52:13 -0600

    Inicio del Proyecto

---
+++ Laboratorio_8/src/reactApp/Indice/indice.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './indice.css';
+
+
+export default function Indice() {
+    return (
+        <header>
+            <div id = "indice">Juego de Cartas</div>
+            <div id = "Try">Try it:</div>
+            
+        </header>
+    );
+}
---
+++ Laboratorio_8/src/reactApp/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Indice from "./Indice/indice";
+
+
+ReactDOM.render(<Indice />, document.getElementById("indice"));
